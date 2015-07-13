db168ca1073c5991222499184848dfe3d5ad19ed
alacambra <alacambra@gmail.com>
2015-07-13 07:30:48 +0200

    added method to show exceptions

--- src/main/java/com/rha/presentation/ResourceController.java
+++ src/main/java/com/rha/presentation/ResourceController.java
@@ -9,14 +9,11 @@ import com.rha.entity.Step;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
-import java.time.temporal.WeekFields;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,11 +25,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import org.primefaces.event.CellEditEvent;
-import org.primefaces.model.chart.Axis;
-import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
-import org.primefaces.model.chart.CategoryAxis;
-import org.primefaces.model.chart.ChartSeries;
 
 public abstract class ResourceController<K, V extends PeriodWithValue> implements Serializable {
 
@@ -169,72 +162,15 @@ public abstract class ResourceController<K, V extends PeriodWithValue> implement
     }
 
     protected void createResourcesGraph() {
-        resourcesGraph = new BarChartModel();
-        ChartSeries total = new ChartSeries();
-        total.setLabel("Estimation of required work resources");
-
-        int size = resourcesRows.size() * periods.size();
-
-        if (size < 1200) {
-
-            resourcesRows.stream().forEach(row -> {
-
-                ChartSeries chartSerie = new ChartSeries();
-                chartSerie.setLabel(row.getTitle());
-
-                row.getResources().stream().forEach(resource -> {
-
-                    String columnName;
-                    long booked = Optional.ofNullable(resource.getValue()).orElse(0L);
-
-                    if (step == Step.WEEK) {
-                        WeekFields fields = WeekFields.of(Locale.GERMANY);
-                        int kw = resource.getStartDate().get(fields.weekOfYear());
-                        columnName = "CW" + kw;
-                    } else {
-                        columnName = Utils.defaultDateFormat(LocalDateConverter.toDate(resource.getStartDate()));
-                    }
-
-                    Double d = 100D;
-                    chartSerie.set(columnName, booked);
-                });
-
-                resourcesGraph.addSeries(chartSerie);
-            });
-        } else {
-            ChartSeries chartSerie = new ChartSeries();
-            chartSerie.setLabel("total");
-
-            int i = 0;
-            totalResources.stream().forEach((value) -> {
-                chartSerie.set(value.getStartDate(), value.getTotal());
-            });
-            resourcesGraph.addSeries(chartSerie);
-        }
-
-        resourcesGraph.setTitle(getResourcesGraphTitle());
-        resourcesGraph.setLegendPosition("ne");
-        resourcesGraph.setStacked(true);
-        resourcesGraph.setShowPointLabels(true);
-        resourcesGraph.setZoom(true);
-
-        Axis xAxis = new CategoryAxis("Period (" + step.name().toLowerCase() + ")");
-        xAxis.setTickAngle(90);
-
-        resourcesGraph.getAxes().put(AxisType.X, xAxis);
-        Axis yAxis = resourcesGraph.getAxis(AxisType.Y);
-
-        yAxis.setLabel("Resources (hours)");
-        
-        
-//         resourcesGraph = new ResourcesGraph<K, V>()
-//                    .setGraphTitle(getResourcesGraphTitle())
-//                    .setPeriods(periods)
-//                    .setResourcesRows(resourcesRows)
-//                    .setTotalResources(totalResources)
-//                    .createResourcesGraph();
+        resourcesGraph = new ResourcesGraph<K, V>()
+                .setGraphTitle(getResourcesGraphTitle())
+                .setPeriods(periods)
+                .setResourcesRows(resourcesRows)
+                .setTotalResources(totalResources)
+                .setStep(step)
+                .createResourcesGraph();
     }
-    
+
     protected abstract String getResourcesGraphTitle();
 
     protected abstract List<PeriodTotal> getTotalResourcesInPeriod();
@@ -297,5 +233,10 @@ public abstract class ResourceController<K, V extends PeriodWithValue> implement
 
     public List<Step> getSteps() {
         return Arrays.asList(Step.values());
+    }
+
+    protected void ShowError(Exception e) {
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getClass().getSimpleName(), e.getMessage());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 }
