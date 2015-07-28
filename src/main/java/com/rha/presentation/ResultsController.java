140232ac7519a3f10d437e841b841a53e847cea5
alacambra <alacambra@gmail.com>
2015-07-28 08:51:20 +0200

    testing double stacked series chart

--- src/main/java/com/rha/presentation/ResultsController.java
+++ src/main/java/com/rha/presentation/ResultsController.java
@@ -40,22 +40,22 @@ import org.primefaces.model.chart.ChartSeries;
 @SessionScoped
 @Named
 public class ResultsController implements Serializable {
-
+    
     @Inject
     transient Logger logger;
-
+    
     @Inject
     transient ResultsFacade resultsFacade;
-
+    
     @Inject
     transient ServiceFacade serviceFacade;
-
+    
     @Inject
     transient CalendarPeriodsGenerator calendarPeriodsGenerator;
-
+    
     @Inject
     transient CalendarEntriesGenerator calendarEntriesGenerator;
-
+    
     List<LocalDate[]> periods;
     List<ResourcesRow<Service, PeriodWithValue>> resultRows;
     List<PeriodTotal> totalResources;
@@ -63,32 +63,32 @@ public class ResultsController implements Serializable {
     LocalDate endDate = LocalDate.now().plusMonths(3).with(TemporalAdjusters.lastDayOfMonth());
     Step step = Step.BIWEEK;
     BarChartModel resourcesChart;
-
+    
     public void loadAvailableResourcesForPeriod() {
-
+        
         Map<Service, List<PeriodWithValue>> remainingResources
                 = resultsFacade.getWeighedRemainingResourcesByService(startDate, endDate);
-
+        
         List<Service> emptyServices = serviceFacade.findAll();
-
+        
         emptyServices.stream().forEach(servcie -> {
             remainingResources.putIfAbsent(servcie, new ArrayList<>());
         });
-
+        
         resultRows = new ArrayList<>();
-
+        
         if (periods == null) {
             loadPeriods();
         }
-
+        
         for (Service service : remainingResources.keySet()) {
-
+            
             Supplier<PeriodWithValue> supplier = () -> {
                 RemainingResource ar = new RemainingResource();
                 ar.setService(service);
                 return ar;
             };
-
+            
             BinaryOperator<PeriodWithValue> reducer = (p1, p2) -> {
                 if (p1 == null) {
                     return p2;
@@ -96,7 +96,7 @@ public class ResultsController implements Serializable {
                 p1.setValue(p1.getValue() + p2.getValue());
                 return p1;
             };
-
+            
             Map<LocalDate, PeriodWithValue> p = remainingResources.get(service).stream()
                     .collect(
                             groupingBy(
@@ -104,18 +104,18 @@ public class ResultsController implements Serializable {
                                     reducing(null, reducer)
                             )
                     );
-
+            
             remainingResources.put(service, p.values().stream().sorted(new PeriodComparator()).collect(toList()));
-
+            
             List<PeriodWithValue> resources = calendarEntriesGenerator
                     .getCalendarEntries(remainingResources.get(service), periods, supplier);
-
+            
             ResourcesRow resourcesRow = new ResourcesRow<>(resources, service);
             resourcesRow.setTitle(service.getName());
             resultRows.add(resourcesRow);
         }
     }
-
+    
     private void loadPeriods() {
         periods = calendarPeriodsGenerator
                 .setStartDate(startDate)
@@ -123,73 +123,73 @@ public class ResultsController implements Serializable {
                 .setStep(step)
                 .generatePeriods();
     }
-
+    
     private void resetValues() {
         resultRows = null;
         periods = null;
         totalResources = null;
         resourcesChart = null;
     }
-
+    
     public List<ResourcesRow<Service, PeriodWithValue>> getAvailableResourceRows() {
         if (resultRows == null) {
             loadAvailableResourcesForPeriod();
         }
-
+        
         return resultRows;
     }
-
+    
     public List<LocalDate> getPeriods() {
-
+        
         if (periods == null) {
             loadPeriods();
         }
-
+        
         return periods.stream().map(period -> period[0]).collect(toList());
     }
-
+    
     private LocalDate getDate(BookedResource br) {
         return br.getStartDate();
     }
-
+    
     public BarChartModel getAreaModel() {
-
+        
         if (resourcesChart == null) {
             loadAvailableResourcesForPeriod();
-//            createAreaModel();
-            resourcesChart = new ResourcesChart<Service, PeriodWithValue>()
-                    .setGraphTitle("Remaining resources")
-                    .setPeriods(periods)
-                    .setResourcesRows(resultRows)
-                    .setTotalResources(totalResources)
-                    .setStep(step)
-                    .setLocale(Locale.GERMANY)
-                    .setExetender("ext")
-                    .createResourcesGraph();
-
-            resourcesChart.setStacked(false);
+            createAreaModel();
+//            resourcesChart = new ResourcesChart<Service, PeriodWithValue>()
+//                    .setGraphTitle("Remaining resources")
+//                    .setPeriods(periods)
+//                    .setResourcesRows(resultRows)
+//                    .setTotalResources(totalResources)
+//                    .setStep(step)
+//                    .setLocale(Locale.GERMANY)
+//                    .setExetender("ext")
+//                    .createResourcesGraph();
+//
+//            resourcesChart.setStacked(false);
         }
-
+        
         return resourcesChart;
     }
-
+    
     private void createAreaModel() {
         resourcesChart = new BarChartModel();
-        ChartSeries total = new ChartSeries();
-        total.setLabel("Estimation of required work resources");
-
+        resourcesChart.setSeriesColors("62656a,a5a5a5,929397,62656a,a5a5a5,929397,62656a,a5a5a5,62656a,a5a5a5,929397,62656a,a5a5a5,929397,62656a,a5a5a5");
         int size = resultRows.size() * periods.size();
-
+        
         WrappedMuttableValue<Float> max = new WrappedMuttableValue<>(0f);
         WrappedMuttableValue<Float> min = new WrappedMuttableValue<>(0f);
-
+        
         if (size < 1200) {
-
+            
             resultRows.stream().forEach(row -> {
-
-                ChartSeries chartSerie = new ChartSeries();
-                chartSerie.setLabel(row.getKey().getName());
-
+                
+                ChartSeries chartSeriePositive = new ChartSeries();
+                chartSeriePositive.setLabel(row.getKey().getName() + "+");
+                ChartSeries chartSerieNegative = new ChartSeries();
+                chartSerieNegative.setLabel(row.getKey().getName());
+                
                 row.getResources().stream().forEach(remainingresource -> {
                     float remainingResources = Optional.ofNullable(remainingresource.getValue()).orElse(0f);
                     if (remainingResources < min.get()) {
@@ -198,7 +198,7 @@ public class ResultsController implements Serializable {
                     if (remainingResources > max.get()) {
                         max.set(remainingResources);
                     }
-
+                    
                     String columnName;
                     if (step == Step.WEEK) {
                         WeekFields fields = WeekFields.of(Locale.GERMANY);
@@ -207,71 +207,78 @@ public class ResultsController implements Serializable {
                     } else {
                         columnName = Utils.defaultDateFormat(LocalDateConverter.toDate(remainingresource.getStartDate()));
                     }
-
-                    chartSerie.set(columnName, remainingResources);
+                    
+                    if (remainingResources >= 0) {
+                        chartSeriePositive.set(columnName, remainingResources);
+                        chartSerieNegative.set(columnName, 0);
+                    } else {
+                        chartSerieNegative.set(columnName, remainingResources);
+                        chartSeriePositive.set(columnName, 0);
+                    }
                 });
-
-                resourcesChart.addSeries(chartSerie);
+                
+                resourcesChart.addSeries(chartSeriePositive);
+                resourcesChart.addSeries(chartSerieNegative);
+                
             });
         } else {
             ChartSeries chartSerie = new ChartSeries();
             chartSerie.setLabel("total");
-
-            int i = 0;
+            
             totalResources.stream().forEach((booking) -> {
                 chartSerie.set(booking.getStartDate(), booking.getTotal());
             });
             resourcesChart.addSeries(chartSerie);
         }
-
+        
         resourcesChart.setExtender("ext");
         resourcesChart.setTitle("Remaining resources");
         resourcesChart.setLegendPosition("ne");
-        resourcesChart.setStacked(false);
+        resourcesChart.setStacked(true);
         resourcesChart.setShowPointLabels(true);
-        resourcesChart.setZoom(true);
-
+//        resourcesChart.setZoom(true);
+        
         Axis xAxis = new CategoryAxis("Period (" + step.name().toLowerCase() + ")");
         xAxis.setTickAngle(0);
-
+        
         resourcesChart.getAxes().put(AxisType.X, xAxis);
         Axis yAxis = resourcesChart.getAxis(AxisType.Y);
-
+        
         yAxis.setLabel("Resources (hours)");
-        yAxis.setMin(Math.round(min.get() * 1.1));
-        yAxis.setMax(Math.round(max.get() * 1.1));
+//        yAxis.setMin(Math.round(min.get() * 1.1));
+//        yAxis.setMax(Math.round(max.get() * 1.1));
     }
-
+    
     public Date getStartDate() {
         return LocalDateConverter.toDate(startDate);
     }
-
+    
     public void setStartDate(Date startDate) {
         this.startDate = LocalDateConverter.toLocalDate(startDate);
     }
-
+    
     public Date getEndDate() {
         return LocalDateConverter.toDate(endDate);
     }
-
+    
     public void setEndDate(Date endDate) {
         this.endDate = LocalDateConverter.toLocalDate(endDate);
     }
-
+    
     public void dateChanged() {
         resetValues();
     }
-
+    
     public Step getStep() {
         return step;
     }
-
+    
     public void setStep(Step step) {
         this.step = step;
     }
-
+    
     public List<Step> getSteps() {
         return Arrays.asList(Step.values());
     }
-
+    
 }
