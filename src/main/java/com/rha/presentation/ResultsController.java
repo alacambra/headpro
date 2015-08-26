9e53e2274289d37ae3738f0ff4f1e3c5f21cc9fb
alacambra <alacambra@gmail.com>
2015-08-26 21:24:28 +0200

    new chart added

--- src/main/java/com/rha/presentation/ResultsController.java
+++ src/main/java/com/rha/presentation/ResultsController.java
@@ -17,11 +17,9 @@ import com.rha.entity.Service;
 import com.rha.entity.Step;
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.time.temporal.TemporalAdjusters;
 import java.time.temporal.WeekFields;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -75,6 +73,7 @@ public class ResultsController implements Serializable {
     List<PeriodTotal> totalResources;
     BarChartModel resourcesChart;
     BarChartModel chart2;
+    BarChartModel chartTotalDifference;
 
     public void loadAvailableVsRequiredResources() {
 
@@ -135,11 +134,74 @@ public class ResultsController implements Serializable {
 
         chart2.addSeries(available);
         chart2.addSeries(required);
+        chart2.setTitle("Available vs required");
+    }
+
+    public void loadTotalDifferenceResources() {
+
+        List<AvailableResource> availableResources
+                = availableResourceFacade.getAvailableResourcesInPeriod(periodController.getLocalStartDate(), periodController.getLocalEndDate());
+
+        List<BookedResource> bookedResources
+                = bookedResourceFacade.getBookedResourcesInPeriod(periodController.getLocalStartDate(), periodController.getLocalEndDate());
+
+        List<AvailableResource> res = calendarEntriesGenerator
+                .getCalendarEntries(availableResources, periodController.getPeriods(), () -> {
+                    AvailableResource ar = new AvailableResource();
+                    ar.setPersisted(false);
+                    return ar;
+                });
+
+        Map<LocalDate, Float> allAvailableResources = res.stream().collect(
+                groupingBy(
+                        PeriodWithValue::getStartDate,
+                        mapping(PeriodWithValue::getValue,
+                                reducing(0f, Float::sum))
+                )
+        );
+
+        bookedResources = calendarEntriesGenerator
+                .getCalendarEntries(bookedResources, periodController.getPeriods(), () -> {
+                    BookedResource ar = new BookedResource();
+                    ar.setPersisted(false);
+                    return ar;
+                });
+
+        Map<LocalDate, Float> allBookedResources = bookedResources.stream().collect(
+                groupingBy(
+                        PeriodWithValue::getStartDate,
+                        mapping(PeriodWithValue::getValue,
+                                reducing(0f, Float::sum))
+                )
+        );
+
+        chartTotalDifference = new BarChartModel();
+        chartTotalDifference.setTitle("Remaining resources");
+        chartTotalDifference.setExtender("ext");
+        ChartSeries remaining = new BarChartSeries();
+
+        periodController.getPeriods().stream().map(period -> period[0]).forEach(date -> {
+            remaining.set(Utils.defaultDateFormat(
+                    LocalDateConverter.toDate(date)),
+                    allAvailableResources.get(date) - allBookedResources.get(date));
+
+        });
+
+        chartTotalDifference.addSeries(remaining);
     }
 
     public BarChartModel getChart2() {
-        loadAvailableVsRequiredResources();
+        if (chart2 == null) {
+            loadAvailableVsRequiredResources();
+        }
         return chart2;
+    }
+
+    public BarChartModel getChartTotalDifference() {
+        if (chartTotalDifference == null) {
+            loadTotalDifferenceResources();
+        }
+        return chartTotalDifference;
     }
 
     public void loadAvailableResourcesForPeriod() {
@@ -194,6 +256,8 @@ public class ResultsController implements Serializable {
         resultRows = null;
         totalResources = null;
         resourcesChart = null;
+        chart2 = null;
+        chartTotalDifference = null;
     }
 
     public List<ResourcesRow<Service, PeriodWithValue>> getAvailableResourceRows() {
@@ -275,7 +339,7 @@ public class ResultsController implements Serializable {
         });
 
         resourcesChart.setExtender("ext");
-        resourcesChart.setTitle("Remaining resources");
+        resourcesChart.setTitle("Remaining resources per Project");
         resourcesChart.setLegendPosition("ne");
         resourcesChart.setStacked(true);
         resourcesChart.setShowPointLabels(true);
